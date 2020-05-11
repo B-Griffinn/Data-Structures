002b965f7e47722677873980bd12db732b9c8023
Ben <bigriffin94@outlook.com>
2020-05-11 16:04:10 -0700

    half way through MVP

--- stack/singly_linked_list.py
+++ stack/singly_linked_list.py
@@ -23,7 +23,7 @@ class Node:
 # ll.next_node.new_node = Node(3)
 
 ########### END OF NODE CLASS ###########
-#############################
+
 ########### BEGIN LINKED LIST ###########
 
 
@@ -32,7 +32,6 @@ class LinkedList:
         # first node in the list
         self.head = None
 
-###### ADD TO TAIL ######
     # add to the end of our list
     def add_to_tail(self, value):
         # regardless if the list is empty or not, we need to wrap the value in a node
@@ -52,14 +51,11 @@ class LinkedList:
             # we are at the end of the linked list
             # current is now the last node in the list so we need to set the new node to be currents next_node
             current.set_next(new_node)
-###### END ADD TO TAIL ######
 
-#############################
-
-###### ADD TO HEAD ######
     def add_to_head(self, value):
         # regardless if the list is empty or not, we need to wrap the value in a node
         new_node = Node(value)
+
         # what if list is empty?
         if not self.head:
             self.head = new_node
@@ -70,13 +66,9 @@ class LinkedList:
             # we can get the head from self.head
             # change current head pointer to be next
             current = self.head
-            current.set_next(None)  # TODO replace with heads next if there
+            current.set_next(None)
             self.head = new_node
-###### END ADD TO HEAD ######
 
-#############################
-
-###### REMOVE FROM HEAD ######
     # remove node from the head of list
     def remove_head(self):
         # what if the list is empty?
@@ -90,28 +82,3 @@ class LinkedList:
             # update self.head
             self.head = self.head.get_next()
             return value
-###### END REMOVE FROM HEAD ######
-
-###############################
-
-###### REMOVE FROM TAIL ########
-    # remove node from the tail of list
-    def remove_tail(self, value):
-        # node_to_remove = Node(value)
-        # what if the list is empty?
-        if not self.head:
-            return None
-        # what if the list is NOT empty?
-        else:
-            # create temp variable for our head
-            temp = self.head
-            # loop through entire list until we reach the end (None)
-            while temp is not None:
-                # create new variable for our previous node
-                prev = temp
-                temp = temp.get_next()
-            # once we have reached the end of the list update prev.next to be none, which should remove our original last node.
-            prev.next = None
-
-
-###### END REMOVE FROM TAIL ######
--- stack/stack.py
+++ stack/stack.py
@@ -10,16 +10,31 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+from singly_linked_list import LinkedList
+
+
 class Stack:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = LinkedList()
+        # self.storage = []
 
     def __len__(self):
-        pass
+        return self.size
 
     def push(self, value):
-        pass
+        # in a stack we push to a the head
+        self.storage.add_to_head(value)
+        # self.storage.append(value)
+        self.size += 1
 
-    def pop(self):
-        pass
+    def pop(self, value):
+        if self.size > 0:
+            popped_item = self.storage.remove_tail(value)
+            self.size -= 1
+            return popped_item
+
+
+# nc = Stack()
+
+# print(nc.__len__())
