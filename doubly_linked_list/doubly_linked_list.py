bb02d2f29ae48b4f52531ab3916cff184eb9d565
Ben <bigriffin94@outlook.com>
2020-05-12 14:50:56 -0700

    DLL MVP DONE

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -104,34 +104,84 @@ class DoublyLinkedList:
     the old tail node's next pointer accordingly."""
 
     def add_to_tail(self, value):
-        pass
+        # create new_node with the value passed in
+        new_tail = ListNode(value)
+        # if length is 0 - set head and tail to = new_tail
+        if self.length == 0:
+            self.head = new_tail
+            self.tail = new_tail
+        # otherwise
+        #  - set prev prop on tail to be new_tail
+        #  - set next prop on new_tail to be tail
+        #  - udpate tail to be new node
+        else:
+            self.tail.next = new_tail
+            new_tail.prev = self.tail
+            self.tail = new_tail
+        # increment length
+        self.length += 1
+        # return list
+        return self
 
     """Removes the List's current tail node, making the 
     current tail's previous node the new tail of the List.
     Returns the value of the removed Node."""
 
     def remove_from_tail(self):
-        pass
+        value = self.tail.value
+        self.delete(self.tail)
+        return value
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List."""
 
     def move_to_front(self, node):
-        pass
+        if node is self.head:
+            return None
+        value = node.value
+        self.delete(node)
+        self.add_to_head(value)
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List."""
 
     def move_to_end(self, node):
-        pass
+        if node is self.tail:
+            return None
+        value = node.value
+        self.delete(node)
+        self.add_to_tail(value)
 
     """Removes a node from the list and handles cases where
     the node was the head or the tail"""
 
     def delete(self, node):
-        pass
+        if not self.head and not self.tail:
+            return None
+        self.length -= 1
+        if self.head == self.tail:
+            self.head = None
+            self.tail = None
+        elif self.head is node:
+            self.head = node.next
+            node.delete()
+        elif self.tail is node:
+            self.tail = node.prev
+            node.delete()
+        else:
+            node.delete()
 
     """Returns the highest value currently in the list"""
 
     def get_max(self):
-        pass
+        # start at the head
+        current = self.head
+        # store the current value on a variable so we can udpate when we loop thru list
+        max_val = current.value
+        # loop thru list until we reach end of list
+        while current is not None:
+            if current.value > max_val:
+                # update max_val to be the current value
+                max_val = current.value
+            current = current.next
+        return max_val
