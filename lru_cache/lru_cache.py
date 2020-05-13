ecc23d1aca141adaa1ab6540168cd3768dd8bfe1
Ben <bigriffin94@outlook.com>
2020-05-13 10:22:09 -0700

    LRU CACHE

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,6 @@
+from doubly_linked_list import DoublyLinkedList
+
+
 class LRUCache:
     """
     Our LRUCache class keeps track of the max number of nodes it
@@ -6,8 +9,12 @@ class LRUCache:
     order, as well as a storage dict that provides fast access
     to every node stored in the cache.
     """
+
     def __init__(self, limit=10):
-        pass
+        self.limit = limit
+        # self.size = 0
+        self.order = DoublyLinkedList()
+        self.storage = dict()
 
     """
     Retrieves the value associated with the given key. Also
@@ -16,8 +23,21 @@ class LRUCache:
     Returns the value associated with the key or None if the
     key-value pair doesn't exist in the cache.
     """
+
     def get(self, key):
-        pass
+        # What if key is not in cache - return none
+        if key not in self.storage:
+            return None
+
+        else:
+            # if key is in cache
+            # then move it to MRU
+            # create a placeholder for our node in our storage
+            node = self.storage[key]
+            self.order.move_to_end(node)
+            # return the value
+            # node value is in our tuple sub 1
+            return node.value[1]
 
     """
     Adds the given key-value pair to the cache. The newly-
@@ -29,5 +49,39 @@ class LRUCache:
     want to overwrite the old value associated with the key with
     the newly-specified value.
     """
+
     def set(self, key, value):
-        pass
+        # set scenarios
+
+        # if item/key already exists
+        if key in self.storage:
+            # overwrite value
+            # where is the value stored?
+            # we can get the node from our storages key
+            node = self.storage[key]
+            # the nodes value is a new tuple with the old key and new value
+            node.value = (key, value)
+            # move value to the tail (whic is most recently used)
+            self.order.move_to_end(node)
+            return  # exit function
+
+        # size is at limit
+        if len(self.order) == self.limit:
+            # evict oldest node
+            # we have 2 references to it already - (LL and dict)
+            # remove from LL and dict
+            index_of_oldest = self.order.head.value[0]
+            # now delete the found index
+            del self.storage[index_of_oldest]
+            # once removed from dict
+            self.order.remove_from_head()
+
+        # add newest node to end/tail
+        # size is not a limit
+        # 1a. add it to order
+        # 1b. a tuple is used bc we just need to store key/vals
+        self.order.add_to_tail((key, value))
+
+        # add it to storage
+        # adding the whole node to the stoarage[key] bc it will let us find both from item when getting or moving things around
+        self.storage[key] = self.order.tail
